6ff94c767561e0dac5a03b0e9925e69714ac774a
llreebor <timoschuk.roman@gmail.com>
2024-10-31 17:49:00 +0200

    fix bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,14 @@ function toggleMobileMenu() {
 	const burger = document.querySelector(".burger");
 	const menu = document.querySelector(".menu");
 	const body = document.querySelector("body");
+	const menuLinks = menu.querySelectorAll(".menu__link");
+	menuLinks.forEach((link) =>
+		link.addEventListener("click", () => {
+			menu.classList.remove("active");
+			burger.classList.remove("active");
+			body.classList.remove("locked");
+		})
+	);
 	burger.addEventListener("click", () => {
 		if (!menu.classList.contains("active")) {
 			menu.classList.add("active");
@@ -28,10 +36,9 @@ toggleMobileMenu();
 function stickyHeader() {
 	const root = document.querySelector(":root");
 	const nav = document.querySelector("header");
-	const breakpoint = 33;
+	const breakpoint = 34;
 	const isScrollingPadding = "80px";
 	const isNotScrollingPadding = "140px";
-	var rs = getComputedStyle(root);
 	if (window.scrollY >= breakpoint) {
 		nav.classList.add("sticky");
 		root.style.setProperty("--scrollPadding", isScrollingPadding);
